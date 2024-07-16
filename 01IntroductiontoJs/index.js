94b48215986af902f6a5c71f71892783040ffb58
NILESH <125892730+nilesh0509@users.noreply.github.com>
2024-07-16 20:31:16 +0530

    Update index.js
    this is for user input

--- 01IntroductiontoJs/index.js
+++ 01IntroductiontoJs/index.js
@@ -6,3 +6,14 @@ function addThreeNumbers(a, b, c) {
 }
 let c = addThreeNumbers(1, 2, 54)
 console.log(c)
+
+
+//another way 
+
+var num1 = prompt("Enter the first Number");
+var num2 = prompt("Enter the second Number");
+var sum = num1 + num2;
+
+console.log("your First number is", num1);
+console.log("your Second number is", num2);
+console.log("Sum of both number is", sum);
